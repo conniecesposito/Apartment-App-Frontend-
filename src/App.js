26f8181f1751874f1ab8d1a29ef669ed318fd685
learn <learn@learn19.local>
2018-07-24 17:02:29 -0700

    Apartment-App-Example with Authentication

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+
+import AllApartments from './pages/apartments/All'
+import CreateApartment from './pages/apartments/Create'
+import ShowApartment from './pages/apartments/Show'
+import Login from './components/Login'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import ApartmentContainer from './containers/apartments'
+
+class App extends Component {
+	render() {
+		return (
+			<Router>
+			<div>
+				<Header />
+				<Switch>
+					<Route path="/apartments" component={ApartmentContainer} />
+			    <Route path="/login" component={Login} />
+			    <Route exact path="/" component={ApartmentContainer}/>
+				</Switch>
+				<Footer />
+			</div>
+			</Router>
+		);
+	}
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './resets.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import Login from './components/Login';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
